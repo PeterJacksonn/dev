828627da28bdfba10bf6e7cf8fe1700c7079c655
PeterJacksonn <dubedoutpeter@gmail.com>
2025-12-16 23:55:59 +0000

    structure view similar to IntelliJ

--- env/.config/nvim/lua/pj/lazy/telescope.lua
+++ env/.config/nvim/lua/pj/lazy/telescope.lua
@@ -27,6 +27,13 @@ return {
 
         local builtin = require('telescope.builtin')
 
+
+        vim.keymap.set("n", "<leader>S", function()
+            builtin.lsp_document_symbols({
+                symbols = { "function", "method" },
+            })
+        end, { desc = "Document symbols (functions)" })
+
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()

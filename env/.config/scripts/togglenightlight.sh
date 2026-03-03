d4547e0d69f0b7903395ebd2b33939e7b7f522fc
PeterJacksonn <dubedoutpeter@gmail.com>
2026-03-03 01:48:30 +0000

    hyprsunset stuff... better nightlight & added gamma control to waybar

--- env/.config/scripts/togglenightlight.sh
+++ env/.config/scripts/togglenightlight.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-NIGHT_TEMP=2700 # warm
+NIGHT_TEMP=2700
+DAY_TEMP=6000
 
-if pgrep -x "hyprsunset" > /dev/null; then
-    pkill hyprsunset
-else
-    hyprsunset --temperature $NIGHT_TEMP &
+CURRENT=$(hyprctl hyprsunset temperature 2>/dev/null)
+
+if [ -z "$CURRENT" ]; then
+    hyprsunset --temperature $DAY_TEMP &
+    exit 0
 fi
 
+if [ "$CURRENT" -eq "$DAY_TEMP" ]; then
+    hyprctl hyprsunset temperature $NIGHT_TEMP
+else
+    hyprctl hyprsunset temperature $DAY_TEMP
+fi
